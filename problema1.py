52d79a32bf79ced4ad87d2ebf231474d5d7cd1e7
Juan Sebastián <jsarteaga38@gmail.com>
2023-11-08 19:16:43 -0500

    Añadir función estudiantes

--- problema1.py
+++ problema1.py
@@ -1,23 +1,22 @@
 def sumar_5_enteros():
-    #Definición de variables
     lista = []
-    contadorWhile= 0
+    contadorWhile = 0
     tamano = 5
     suma = 0
 
-    #ingresar los números
     while contadorWhile < tamano:
-        lista.append(int(input("Ingrese número " + str(contadorWhile+1) + ": ")))
+        # Agregar numeros a la lista.
+        lista.append(int(input("Ingrese numero " + str(contadorWhile+1) + ": ")))
         contadorWhile += 1
 
-        #Sumamos los números:
     contadorWhile = 0
     while contadorWhile < tamano:
-            suma += lista[contadorWhile]
-            contadorWhile += 1
+        suma += lista[contadorWhile]
+        contadorWhile += 1
 
+    print("Los elementos de la lista son:")
     for numero in lista:
-        print(numero,end=', ')
+        print(numero,end=", ")
 
     print("\nLa suma de todos sus elementos es:")
     print(suma)
